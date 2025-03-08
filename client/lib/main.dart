6813ac0ff35da5fbb7dad247c5894084f837425d
Shota Ide <ide.shota.dev@gmail.com>
2025-03-08 13:09:12 +0900

    Update main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide User;
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
