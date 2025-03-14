ab26a3ae26ddb0b53da13d01157196ad701f44c6
Shota Ide <ide.shota.dev@gmail.com>
2025-03-14 21:39:52 +0900

    android

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -23,14 +23,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "ide.shota.colomney.HouseWorker.dev"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders = [
+            // TODO: 本番アプリでは無効化する
+                usesCleartextTraffic: "true"
+        ]
     }
 
     buildTypes {
