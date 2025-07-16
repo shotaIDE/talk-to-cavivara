ccb36fe3cf8f68e116d35aa97365b125cdb918f8
Shota Ide <ide.shota.dev@gmail.com>
2025-07-16 22:15:07 +0900

    Update app_feature.dart

--- client/lib/data/definition/app_feature.dart
+++ client/lib/data/definition/app_feature.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:house_worker/data/definition/flavor.dart';
 
-final bool showDebugFeatures = !(flavor == Flavor.prod && kReleaseMode);
-
 /// 右上肩にバナーを表示するか否か
 ///
 /// 一般公開アプリ以外は常に表示する
