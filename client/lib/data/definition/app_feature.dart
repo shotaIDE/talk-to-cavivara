fe30e2b4fb3893691394b14b05e7ce88e3432553
Shota Ide <ide.shota.dev@gmail.com>
2025-07-16 20:02:24 +0900

    refactor

--- client/lib/data/definition/app_feature.dart
+++ client/lib/data/definition/app_feature.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:house_worker/data/definition/flavor.dart';
 
+final bool showDebugFeatures = !(flavor == Flavor.prod && kReleaseMode);
+
+final bool showCustomAppBanner =
+    (flavor == Flavor.prod && !kReleaseMode) || flavor != Flavor.prod;
+
+final bool showAppDebugBanner = !showCustomAppBanner && showDebugFeatures;
+
 final useFirebaseEmulator = flavor == Flavor.emulator;
 
 const bool isAnalyticsEnabled =
