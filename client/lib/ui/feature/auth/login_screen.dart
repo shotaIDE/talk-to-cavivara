2b7359487363154ec80c4a9c81dedbc9d440dea7
Shota Ide <shotaIDE@users.noreply.github.com>
2025-09-22 23:46:05 +0900

    fix: ログイン画面の表示内容を修正 (#41)

--- client/lib/ui/feature/auth/login_screen.dart
+++ client/lib/ui/feature/auth/login_screen.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:house_worker/data/model/sign_in_result.dart';
-import 'package:house_worker/ui/component/color.dart';
+import 'package:house_worker/ui/component/cavivara_avatar.dart';
 import 'package:house_worker/ui/feature/auth/login_presenter.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
@@ -25,51 +21,32 @@ class LoginScreen extends ConsumerStatefulWidget {
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    final startWithGoogleButton = ElevatedButton.icon(
-      onPressed: _startWithGoogle,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-      ),
-      icon: const Icon(FontAwesomeIcons.google),
-      label: const Text('Googleで続ける'),
-    );
-
-    final startWithAppleButton = ElevatedButton.icon(
-      onPressed: _startWithApple,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: signInWithAppleBackgroundColor(context),
-        foregroundColor: signInWithAppleForegroundColor(context),
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-      ),
-      icon: const Icon(FontAwesomeIcons.apple),
-      label: const Text('Appleで続ける'),
-    );
-
-    final continueWithoutAccountButton = TextButton(
+    final continueWithoutAccountButton = ElevatedButton(
       onPressed: _startWithoutAccount,
-      style: TextButton.styleFrom(
+      style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
       ),
-      child: const Text('アカウントを利用せず続ける'),
+      child: const Text('はじめる'),
     );
 
     final children = <Widget>[
-      const Text(
+      Text(
         'カヴィヴァラチャット',
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        style: Theme.of(context).textTheme.headlineMedium,
       ),
-      const SizedBox(height: 20),
-      const Text('家事を簡単に記録・管理できるアプリ', style: TextStyle(fontSize: 16)),
-      const SizedBox(height: 60),
-      startWithGoogleButton,
       const SizedBox(height: 16),
+      const CavivaraAvatar(
+        size: 160,
+      ),
+      const SizedBox(height: 32),
+      Text(
+        'カヴィヴァラさんと\n楽しくおしゃべりしよう',
+        style: Theme.of(context).textTheme.bodyLarge,
+        textAlign: TextAlign.center,
+      ),
+      const SizedBox(height: 60),
+      continueWithoutAccountButton,
     ];
-
-    if (Platform.isIOS) {
-      children.addAll([startWithAppleButton, const SizedBox(height: 16)]);
-    }
-
-    children.add(continueWithoutAccountButton);
 
     return Scaffold(
       body: Center(
@@ -81,53 +58,9 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     );
   }
 
-  Future<void> _startWithGoogle() async {
-    try {
-      await ref.read(startResultProvider.notifier).startWithGoogle();
-    } on SignInWithGoogleException catch (error) {
-      if (!mounted) {
-        return;
-      }
-
-      switch (error) {
-        case SignInWithGoogleExceptionCancelled():
-          return;
-        case SignInWithGoogleExceptionUncategorized():
-          ScaffoldMessenger.of(context).showSnackBar(_failedLoginSnackBar);
-          return;
-      }
-    }
-
-    // ホーム画面への遷移は RootApp で自動で行われる
-  }
-
-  Future<void> _startWithApple() async {
-    try {
-      await ref.read(startResultProvider.notifier).startWithApple();
-    } on SignInWithAppleException catch (error) {
-      if (!mounted) {
-        return;
-      }
-
-      switch (error) {
-        case SignInWithAppleExceptionCancelled():
-          return;
-        case SignInWithAppleExceptionUncategorized():
-          ScaffoldMessenger.of(context).showSnackBar(_failedLoginSnackBar);
-          return;
-      }
-    }
-
-    // ホーム画面への遷移は RootApp で自動で行われる
-  }
-
   Future<void> _startWithoutAccount() async {
     await ref.read(startResultProvider.notifier).startWithoutAccount();
 
     // ホーム画面への遷移は RootApp で自動で行われる
   }
 }
-
-const _failedLoginSnackBar = SnackBar(
-  content: Text('ログインに失敗しました。しばらくしてから再度お試しください。'),
-);
