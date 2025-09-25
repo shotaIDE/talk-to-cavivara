325e0f0c78677990ebcd156417e65136d619eaf7
Shota Ide <ide.shota.dev@gmail.com>
2025-09-25 23:55:47 +0900

    Update fire_confirmation_dialog.dart

--- client/lib/ui/component/fire_confirmation_dialog.dart
+++ client/lib/ui/component/fire_confirmation_dialog.dart
@@ -7,15 +7,18 @@ class FireConfirmationDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('解雇しますか？'),
-      content: const Text('本当に解雇すると、再度雇用するまで相談できなくなります。'),
+      content: const Text('解雇すると、再度雇用するまで会議できなくなります。'),
       actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: Text(
+            '解雇する',
+            style: TextStyle(color: Theme.of(context).colorScheme.error),
+          ),
+        ),
         TextButton(
           onPressed: () => Navigator.of(context).pop(false),
           child: const Text('キャンセル'),
-        ),
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(true),
-          child: const Text('解雇する'),
         ),
       ],
     );
